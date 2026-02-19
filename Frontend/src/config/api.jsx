5dc81459061fc977cfa811210484f5b495017d05
srinija madireddy <150924833+srinijamadireddy19@users.noreply.github.com>
2026-02-19 11:56:06 +0530

    Update api.jsx

--- Frontend/src/config/api.jsx
+++ Frontend/src/config/api.jsx
@@ -5,7 +5,7 @@ const API_CONFIG = {
   // Base URL for the API
   // In development, this will use the proxy defined in package.json
   // In production, set this to your actual API URL
-  BASE_URL: "https://you-tube-video-summarizer-khaki.vercel.app/" || '',
+  BASE_URL: "https://you-tube-video-summarizer-khaki.vercel.app" ,
   
   // API Endpoints
   ENDPOINTS: {
@@ -18,3 +18,4 @@ const API_CONFIG = {
 
 
 export default API_CONFIG;
+
